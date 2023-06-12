81c94f8aefeeec5c3c88ce3a0e166f6bd9862b96
Matt Oklahuman <theoklahuman@gmail.com>
2023-06-11 19:57:33 -0500

    More work on page

--- src/index.js
+++ src/index.js
@@ -3,22 +3,25 @@ import "./style.css";
 const button = document.querySelector("button");
 const ideaDisplay = document.querySelector(".idea-display");
 
-button.addEventListener("click", () => {
-    console.log("it works!");
-    getIdea();
-}
-);
+button.addEventListener("click", displayIdea);
 
 async function getIdea() {
     const idea = await fetch("https://www.boredapi.com/api/activity?", {
         mode: "cors",
     });
     let response = await idea.json();
-    ideaDisplay.textContent = response.activity;
-    displayIdea();
+    return response.activity;
 }
 
 function displayIdea() {
-    // document.body.classList.add("upbeat-theme");
+    getIdea().then((value) => {
+        ideaDisplay.textContent = value;
+    });
     ideaDisplay.classList.add("visible");
 }
+
+// function addIdeaInformation() {
+//     const info = document.createElement("div");
+//     info.textContent = "this is a test";
+//     ideaDisplay.insertAdjacentElement("afterend", info);
+// }
